6e6997a225d0912a26bbe2709c5ba5c702c60374
Yifan <372104922@qq.com>
2022-02-13 23:05:24 +0800

    chore: class类组件

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './assets/icons/logo.svg';
 import robots from './mockdata/robot.json'
 import Robot from './components/Robot';
 import styles from './App.module.css';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <img src={logo} alt="logo" className={styles.appLogo} />
         <h1>罗伯特机器人选课吊炸天online购物平台我好长啊啊啊啊啊</h1>
       </div>
+      <ShoppingCart />
       <div className = {styles.robotList}>
         {robots.map( (r) => (
         <Robot id={r.id} email={r.email} name={r.name}/>
