43304841c50891da507b11ff564ddaa0d3f3b6d4
Yifan <372104922@qq.com>
2022-02-13 22:28:09 +0800

    chore: 加载媒体与字体

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/icons/logo.svg';
 import robots from './mockdata/robot.json'
 import Robot from './components/Robot';
 import styles from './App.module.css';
@@ -7,6 +7,10 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} alt="logo" className={styles.appLogo} />
+        <h1>罗伯特机器人选课吊炸天online购物平台我好长啊啊啊啊啊</h1>
+      </div>
       <div className = {styles.robotList}>
         {robots.map( (r) => (
         <Robot id={r.id} email={r.email} name={r.name}/>
